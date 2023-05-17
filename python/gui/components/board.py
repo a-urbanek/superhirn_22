af0c6ee13778cdcaa9d4223fe080d753e2fb7e97
frievoe97 <friedrich.voelkers@me.com>
2023-05-17 14:12:24 +0200

    Added folder structure (second version) \w Adrian Urbanek

---
+++ python/gui/components/board.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Board:
+    def __init__(self):
+        """
+        Initialisiert das Spielbrett.
+        """
+
+    def draw(self, screen):
+        """
+        Zeichnet das Spielbrett auf den Bildschirm.
+        """
+
+    # Weitere Methoden für das Spielbrett
+
---
+++ python/gui/components/menu.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Menu:
+    def __init__(self):
+        """
+        Initialisiert das Menü.
+        """
+
+    def draw(self, screen):
+        """
+        Zeichnet das Menü auf den Bildschirm.
+        """
+
+    # Weitere Methoden für das Menü
+
