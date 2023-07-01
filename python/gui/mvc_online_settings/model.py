79d2fca85ff2e9f0f679ca20c7533cbca5b41dbd
Robert Berg <47332521+robert-berg@users.noreply.github.com>
2023-07-01 11:15:30 +0200

    sync supermode

--- python/gui/mvc_online_settings/model.py
+++ python/gui/mvc_online_settings/model.py
@@ -1,7 +1,26 @@
+import sys
+sys.path.insert(0, '..')
+
+from config import config
+
 class Model:
     def __init__(self):
 
-        self.super_mode = False
+        self._super_mode = False
         self.online_mode = False
         self.port = ""
         self.ip_address = ""
+        self.update_super_mode()
+
+    @property
+    def super_mode(self):
+        return self._super_mode
+
+    @super_mode.setter
+    def super_mode(self, value):
+        self._super_mode = value
+        self.update_super_mode()
+
+    def update_super_mode(self):
+        config.IS_SUPERHIRN = self._super_mode
+        print("update")
