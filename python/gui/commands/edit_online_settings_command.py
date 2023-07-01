e570dbea97b41cff74c8e28cf2222bc6217e8011
Robert Berg <r.berg.2001@gmail.com>
2023-07-01 22:19:48 +0200

    change !online setting" to "settings"

--- python/gui/commands/edit_online_settings_command.py
+++ python/gui/commands/edit_online_settings_command.py
@@ -10,8 +10,8 @@ from constants import ONLINE_SETTINGS
 class EditOnlineSettings(Command):
     
     def execute(self):
-        print("Online Settings")
+        print("Settings")
         self.main_app.set_state(ONLINE_SETTINGS)
 
     def __str__(self):
-        return "Online Settings"
+        return "Settings"
